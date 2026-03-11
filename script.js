69031fced654954552b0a0a979d70af7dbffce61
anajungg <ana_l_jung@estudante.sesisenai.org.br>
2026-03-11 08:59:40 -0300

    ADD- Iniciei

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Hello World")
+
+let nome = "Ana";
+console.log(nome);
+
+const pi = 3.14;
+console.log(pi);
+
+let n1 = 1;
+let n2 = 2;
+console.log(n1+n2);
+
+function saudacao(){
+console.log("Olá meu nome é "+nome);
+};
+
+function somar(n3,n4) {
+    console.log(n3 + n4)
+};
+saudacao();
+somar(2,3);
